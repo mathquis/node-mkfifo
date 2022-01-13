34f139d9ea8d5fbc9c9a22107fd6e3f5382a887b
Mathieu Quisefit <quisefit@wcentric.com>
2022-01-13 20:01:24 +0100

    Removed log

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 const Path			= require('path')
 const Binary		= require('@mapbox/node-pre-gyp')
-console.log(Path.resolve(Path.join(__dirname,'./package.json')))
 const binding_path	= Binary.find(Path.resolve(Path.join(__dirname,'./package.json')))
 
 module.exports = require(binding_path)
